43105038909dc86922254208fd79ae4e486c2fd9
Fynn <cx495086@outlook.com>
2019-11-02 23:54:22 +0800

    init

---
+++ app/src/main/java/com/app/cx/testapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.app.cx.testapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val ITEM_CLASS= "com.app.cx.item"
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /***点击添加按键执行**/
+    fun addItm(view: View) {
+        //
+        val addText=findViewById<TextView>(R.id.textView)
+        val inputText=addText.text.toString()
+        Log.e("add",inputText)
+    }
+}
